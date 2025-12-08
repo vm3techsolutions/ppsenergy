029dcbe8e65b375e89f21a9932c95807e09f1045
Devyani Dayanand Anandgaonkar <devyanigitanand02@gmail.com>
2025-12-08 15:54:54 +0530

    Updated Associates & Mentors Page

---
+++ client/src/app/about/key-resources/associates-and-mentors/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './Banner'
+import AssociatesMentorsTeam from './AssociatesMentorsTeam'
+
+export default function page() {
+  return (
+   <>
+   <Banner/>
+   <AssociatesMentorsTeam/>
+   </>
+  )
+}
