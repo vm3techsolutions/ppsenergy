bd9910ab031ea183befa1764c8fedd26bb4cf93c
Devyani Dayanand Anandgaonkar <devyanigitanand02@gmail.com>
2026-01-20 13:26:48 +0530

    Image updated in service page

--- client/src/app/services/power-consultancy/ComprehensivePowerConsultancy.jsx
+++ client/src/app/services/power-consultancy/ComprehensivePowerConsultancy.jsx
@@ -52,7 +52,7 @@ export default function ComprehensivePowerConsultancy() {
           <Image
                     width={678}
                     height={574}
-                    src="/assets/services/powerConsultancy/powerConsultancyInfo.jpeg"
+                    src="/assets/services/powerConsultancy/ComprehensiveBannerPowerConsultancy.jpeg"
                     alt="About Us"
                     className="w-full max-w-md md:max-w-full h-[350px] lg:h-[450px] rounded-bl-[120px] rounded-tl-[40px] shadow-lg object-cover"/>
          
